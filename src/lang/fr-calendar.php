b2fec9b5f845faae1a463868dca1cb3ceed09e9d
connorwilkins <connormw@gmail.com>
2016-01-24 13:49:53 -0500

    Create fr-calendar.php
    French calendar translation. Didn't know how to add a folder into fork on GH.

---
+++ src/lang/fr-calendar.php
@@ -0,0 +1,56 @@
+<?php
+return array(
+    /*
+    |--------------------------------------------------------------------------
+    | Calendar Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines for the CalendarGenerator
+    |
+    */
+    'su'         => 'D',
+    'mo'         => 'L',
+    'tu'         => 'Ma',
+    'we'         => 'Me',
+    'th'         => 'J',
+    'fr'         => 'V',
+    'sa'         => 'S',
+    'sun'        => 'Dim',
+    'mon'        => 'Lu',
+    'tue'        => 'Ma',
+    'wed'        => 'Me',
+    'thu'        => 'Jeu',
+    'fri'        => 'Vend',
+    'sat'        => 'Sam',
+    'sunday'     => 'Dim',
+    'monday'     => 'Lundi',
+    'tuesday'    => 'Mardi',
+    'wednesday'  => 'Mecredi',
+    'thursday'   => 'Jeudi',
+    'friday'     => 'Vendredi',
+    'saturday'   => 'Samedi',
+    'jan'        => 'Janv',
+    'feb'        => 'Févr',
+    'mar'        => 'Mars',
+    'apr'        => 'Avril',
+    'may'        => 'Mai',
+    'jun'        => 'Juin',
+    'jul'        => 'Juil',
+    'aug'        => 'Août',
+    'sep'        => 'Sept',
+    'oct'        => 'Oct',
+    'nov'        => 'Nov',
+    'dec'        => 'Déc',
+    'january'    => 'Janvier',
+    'february'   => 'Février',
+    'march'      => 'Mars',
+    'april'      => 'Avril',
+    'mayl'       => 'Mai',
+    'june'       => 'Juin',
+    'july'       => 'Juillet',
+    'august'     => 'Août',
+    'september'  => 'Septembre',
+    'october'    => 'Octobre',
+    'november'   => 'Novembre',
+    'december'   => 'Décembre'
+);
